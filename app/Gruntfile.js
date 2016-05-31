d4b0421af9a427088831cf7b5b507fc0ff9b3842
wenzhang <zhangwenting111@gmail.com>
2016-05-30 21:57:39 -0400

    fix grunt

--- app/Gruntfile.js
+++ app/Gruntfile.js
@@ -7,13 +7,7 @@ module.exports = function(grunt) {
                     style: 'expanded'
                 },
                 files: {
-                    'css/style1.css': 'scss/style1.scss',
-                    'css/style2.css': 'scss/style2.scss',
-                    'css/style3.css': 'scss/style3.scss',
-                    'css/style4.css': 'scss/style4.scss',
-                    'css/style5.css': 'scss/style5.scss',
-                    'css/style6.css': 'scss/style6.scss',
-                    'css/simple.css': 'scss/simple.scss'
+                    'css/app.css': 'scss/app.scss'
                 }
             }
         },
@@ -23,13 +17,7 @@ module.exports = function(grunt) {
             },
             dist: {
                 files: {
-                    'css/style1.css': 'css/style1.css',
-                    'css/style2.css': 'css/style2.css',
-                    'css/style3.css': 'css/style3.css',
-                    'css/style4.css': 'css/style4.css',
-                    'css/style5.css': 'css/style5.css',
-                    'css/style6.css': 'css/style6.css',
-                    'css/simple.css': 'css/simple.css'
+                    'css/app.css': 'css/app.css'
                 }
             }
         },
@@ -37,11 +25,23 @@ module.exports = function(grunt) {
             css: {
                 files: 'scss/*.scss',
                 tasks: ['sass', 'autoprefixer']
+            },
+            options: {
+              livereload: true
             }
+        },
+        connect: {
+          server: {
+            options: {
+              port: 8080,
+              hostname: '*'
+            }
+          }
         }
     });
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-autoprefixer');
-    grunt.registerTask('default', ['watch']);
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.registerTask('default', ['connect', 'watch']);
 }
