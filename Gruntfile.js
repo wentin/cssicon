0aac15dd41f62c3a1b9dfec7235afaa7444dcebd
wenzhang <zhangwenting111@gmail.com>
2016-09-20 21:23:08 -0400

    change folder

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                options: {
+                    style: 'expanded'
+                },
+                files: {
+                    'css/app.css': 'scss/app.scss',
+                    'css/icons.css': 'scss/icons.scss'
+                }
+            }
+        },
+        autoprefixer: {
+            options: {
+                map: true
+            },
+            dist: {
+                files: {
+                    'css/app.css': 'css/app.css',
+                    'css/icons.css': 'css/icons.css',
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: 'scss/*.scss',
+                tasks: ['sass', 'autoprefixer']
+            },
+            options: {
+              livereload: true
+            }
+        },
+        connect: {
+          server: {
+            options: {
+              port: 8080,
+              hostname: '*'
+            }
+          }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.registerTask('default', ['connect', 'watch']);
+}
