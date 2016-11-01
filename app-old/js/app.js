76a0412a15c13adee3fa5b6ab522a4506b1223b1
wenzhang <zhangwenting111@gmail.com>
2016-11-01 13:13:44 -0400

    remove app-old

--- app-old/js/app.js
+++
@@ -1,6 +0,0 @@
-var app = angular.module("resourcesApp", []);
-app.controller("MainCtrl", ['$scope','$http', function($scope, $http) { 
-	$http.get('js/cssicon.json').success (function(data){ 
-		$scope.icons = data; 
-	}); 
-}] ); 
